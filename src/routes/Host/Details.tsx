c99eaf6c46993c4107f5f6e0f46538694f114cd7
JClark <justindanielclark@gmail.com>
2023-04-06 05:17:57 -0700

    Removed hardcoded data

--- src/routes/Host/Details.tsx
+++ src/routes/Host/Details.tsx
@@ -1,44 +1,28 @@
 import Van from "../../types/Van";
+import Host from "../../types/Host";
+import Review from "../../types/Review";
+
 import { NavLink, useLoaderData } from "react-router-dom";
 
-type LoadedData = {
-  vans: Array<Van>;
-  income: number;
-  reviewScore: number;
-};
-
 async function loader() {
-  return null;
+  //TODO: Currently Hardcoded Search For Host '123'
+  return fetch("/api/hosts/123/details").then((res) => {
+    if (res.status === 200) {
+      return res.json();
+    }
+    throw new Error("Unable To Retrieve API Data");
+  });
 }
+
 export default function Details() {
-  // const data: LoadedData = useLoaderData();
-  const data = {
-    income: 2000,
-    reviewScore: 4.5,
-    vans: [
-      {
-        id: "3",
-        name: "Reliable Red",
-        price: 100,
-        description:
-          "Reliable Red is a van that was made for travelling. The inside is comfortable and cozy, with plenty of space to stretch out in. There's a small kitchen, so you can cook if you need to. You'll feel like home as soon as you step out of it.",
-        imageUrl:
-          "https://assets.scrimba.com/advanced-react/react-router/reliable-red.png",
-        type: "luxury",
-      },
-      {
-        id: "6",
-        name: "Green Wonder",
-        price: 70,
-        description:
-          "With this van, you can take your travel life to the next level. The Green Wonder is a sustainable vehicle that's perfect for people who are looking for a stylish, eco-friendly mode of transport that can go anywhere.",
-        imageUrl:
-          "https://assets.scrimba.com/advanced-react/react-router/green-wonder.png",
-        type: "rugged",
-      },
-    ],
+  const data = useLoaderData() as {
+    hostData: Host;
+    reviews: Array<Review>;
+    vans: Array<Van>;
   };
-  const income = parseFloat(data.income.toFixed(2)).toLocaleString("en-IN", {
+  const income = parseFloat(
+    data.hostData.last30Income.toFixed(2)
+  ).toLocaleString("en-IN", {
     style: "currency",
     currency: "USD",
   });
@@ -61,23 +45,29 @@ export default function Details() {
         </NavLink>
       </section>
       {/* Review */}
-      <section className="flex flex-row bg-orange-100 p-4 flex-wrap gap-1 justify-end">
-        <p className="flex-1">
-          <span className="inline-block text-xl font-bold mr-2 whitespace-nowrap">
-            Review Score:
-          </span>
-          <span className="font-bold whitespace-nowrap">
-            🌟 {data.reviewScore}
-          </span>
-          /5.0
-        </p>
-        <NavLink
-          to="/host/reviews"
-          className="py-1 px-2 bg-teal-800 text-white font-bold rounded h-fit basis-auto text-sm"
-        >
-          Details
-        </NavLink>
-      </section>
+
+      {data.reviews.length > 0 ? (
+        <section className="flex flex-row bg-orange-100 p-4 flex-wrap gap-1 justify-end">
+          <p className="flex-1">
+            <span className="inline-block text-xl font-bold mr-2 whitespace-nowrap">
+              Review Score:
+            </span>
+            <span className="font-bold whitespace-nowrap">
+              🌟{" "}
+              {data.reviews.reduce((acc, cur) => acc + cur.rating, 0) /
+                data.reviews.length}
+            </span>
+            /5.0
+          </p>
+          <NavLink
+            to="/host/reviews"
+            className="py-1 px-2 bg-teal-800 text-white font-bold rounded h-fit basis-auto text-sm"
+          >
+            Details
+          </NavLink>{" "}
+        </section>
+      ) : undefined}
+
       {/* Your Listed Vans */}
       <section className="bg-orange-200 flex-1 p-4">
         <div className="flex flex-row flex-wrap gap-1 justify-end">
@@ -94,7 +84,10 @@ export default function Details() {
         <ul className="flex flex-col gap-2 mt-4">
           {data.vans.map((van) => {
             return (
-              <li className="flex flex-row p-3 gap-4 bg-neutral-50 rounded items-center mx-auto w-full max-w-lg">
+              <li
+                key={van.id}
+                className="flex flex-row p-3 gap-4 bg-neutral-50 rounded items-center mx-auto w-full max-w-lg"
+              >
                 <img
                   className="w-16 h-16 rounded-lg"
                   src={van.imageUrl}
@@ -118,3 +111,5 @@ export default function Details() {
     </>
   );
 }
+
+export { loader };
